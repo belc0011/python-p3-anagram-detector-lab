7fe9e1b1c64567a19b3d3b622361f72078b51510
Monica Garcia <monicairenegarcia@gmail.com>
2024-03-23 18:04:02 -0700

    complete anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        word_letters = [letter for letter in self.word]
+        found_word = []
+        for word in list:
+            letters = [letter for letter in word]
+            if (sorted(word_letters) == sorted(letters)):
+                found_word.append(word)
+        return found_word
